f7c5174825372d27c1cce3e8bd278adf2dc62079
Drigo Alexander <drigosihombinga@gmail.com>
2021-02-28 22:50:38 +0700

    Prizes event changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,11 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+
+    boxShadow: {
+      'neu': ' -4px -4px 7px #1D1D1D, 4px 4px 7px rgba(0, 0, 0, .288)',
+    },
+
     extend: {
       fontFamily:{
         body: ['Poppins'],
@@ -29,7 +34,10 @@ module.exports = {
         table: ['#171848'],
         dashboard: ['#c5e7dd'],
         competable1: ['#ffdfaf'],
-        competable2: ['#feb8b4']
+        competable2: ['#feb8b4'],
+        shadow1: ['#1D1D1D'],
+        shadow2: ['#040404'],
+        captionprize: ['#CBCBCB']
       }
     },
   },
